c2027846e778acf084e5dc195c208521dc18c47a
Vipin Aggarwal <vipin.aggarwal@buildzar.com>
2015-10-07 09:53:52 +0530

    Added pause functinality in recording.

--- Pitch-Perfect/RecordSoundsViewController.swift
+++ Pitch-Perfect/RecordSoundsViewController.swift
@@ -9,36 +9,46 @@
 import UIKit
 import AVFoundation
 
+enum RecordingState {
+    case Stopped
+    case Recording
+    case Paused
+}
+
 
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
-
+    
+    @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopButon: UIButton!
+    @IBOutlet weak var pausePlayButton: UIButton!
+    @IBOutlet weak var finishRecordingLabel: UILabel!
     
     //Declared Globally
     var audioRecorder:AVAudioRecorder!
     var recordedAudio:RecordedAudio!
+    var recordingState = RecordingState.Stopped
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(animated: Bool) {
-        stopButon.hidden = true
+        visualSetupBeforeRecordStart()
     }
     
-
+    
     @IBAction func recordAudio(sender: UIButton) {
         print("in recordAutdio")
-        recordingLabel.hidden = false
-        stopButon.hidden = false
+        visualSetupAfterRecordStart()
+        recordingState = .Recording
         
         //Inside func recordAudio(sender: UIButton)
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
@@ -58,18 +68,44 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.meteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.delegate = self
-        audioRecorder.record() 
+        audioRecorder.record()
+    }
+    
+    
+    
+    @IBAction func pausePlayButtonTapped(sender: AnyObject) {
+        switch recordingState {
+        case .Paused:
+            recordingLabel.text = "Recording"
+            audioRecorder.record()
+            recordingState = .Recording
+            if let playButtonImage = UIImage(named: "pauseButton") {
+                pausePlayButton.setImage(playButtonImage, forState: .Normal)
+            }
+
+        case .Recording:
+            recordingLabel.text = "Paused"
+            audioRecorder.pause()
+            recordingState = .Paused
+            if let playButtonImage = UIImage(named: "playButton") {
+                pausePlayButton.setImage(playButtonImage, forState: .Normal)
+            }
+            
+        default:
+            stopButtonTapped(self)
+        }
     }
     
     
     @IBAction func stopButtonTapped(sender: AnyObject) {
-        print("Stop button pressed")
+        recordingState = .Stopped
         recordingLabel.hidden = true
         
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
     }
+    
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "stopRecording" {
@@ -79,8 +115,9 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
     
+    
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
-
+        
         if (flag) {
             recordedAudio = RecordedAudio();
             recordedAudio.filePathURL = recorder.url
@@ -88,9 +125,31 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
             
             self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
         }
-
+        
     }
-
+    
+    
+    func visualSetupBeforeRecordStart () {
+        recordButton.enabled = true
+        recordingLabel.text = "Tap on Mic to start recording voice"
+        pausePlayButton.hidden = true
+        stopButon.hidden = true
+        finishRecordingLabel.hidden = true
+        
+        if let playButtonImage = UIImage(named: "pauseButton") {
+            pausePlayButton.setImage(playButtonImage, forState: .Normal)
+        }
+        
+    }
+    
+    func visualSetupAfterRecordStart () {
+        recordButton.enabled = false
+        recordingLabel.text = "Recording"
+        pausePlayButton.hidden = false
+        stopButon.hidden = false
+        finishRecordingLabel.hidden = false
+    }
+    
 }
 
  
